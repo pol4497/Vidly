67c202ee875f80ec3ac561d55bbc24cce85dbeb9
Thodoris <thodoris_politidis@yahoo.gr>
2021-03-19 12:26:08 +0200

    Phone Number added to the registration form

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -12,6 +12,11 @@ namespace Vidly.Models
         [Required]
         [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -71,6 +76,11 @@ namespace Vidly.Models
         [Required]
         [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [StringLength(50)]
+        public string Phone { get; set; }
 
         [Required]
         [EmailAddress]
